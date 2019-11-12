cc3876f74b912fe006d8803141651e80728f45f4
yntik <eifaurya@gmail.com>
2019-11-12 11:31:18 +0200

    add Giphy config and first request

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -2,18 +2,25 @@
     <div class="search-container">
         <input class="search-input" v-model.lazy="searchQuery"/>
         <button class="search-button"> Search</button>
+        <img :src="url"/>
     </div>
 </template>
 
 <script>
   import axios from 'axios'
+  import apiGiphyConfig from '../../apiGiphyConfig.json'
 
   export default {
     name: "SearchComponent",
     data() {
       return {
         searchQuery: '',
+        url: ''
       }
+    },
+    async created() {
+      let data = await axios.get(`${apiGiphyConfig.trending}?api_key=${apiGiphyConfig.apiKey}&limit=25`)
+      this.url = data.data.data[0].images['fixed_height']['url']
     }
   }
 </script>
