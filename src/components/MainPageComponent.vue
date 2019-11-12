73edf8491fde457e1f45b7fa698574f78d0f286b
yntik <eifaurya@gmail.com>
2019-11-12 11:04:53 +0200

    add search component

--- src/components/MainPageComponent.vue
+++ src/components/MainPageComponent.vue
@@ -1,19 +1,28 @@
 <template>
-    <ListComponent>
-    </ListComponent>
+    <div class="main-container">
+        <SearchComponent>
+        </SearchComponent>
+        <ListComponent>
+        </ListComponent>
+    </div>
 </template>
 
 <script>
   import ListComponent from './ListComponent.vue'
+  import SearchComponent from './SearchComponent.vue'
 
   export default {
     name: "MainPageComponent",
     components: {
-      ListComponent
+      ListComponent,
+      SearchComponent
     },
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .main-container {
+        max-width: 1140px;
+        margin: 0 auto;
+    }
 </style>
---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="search-container">
+        <input class="search-input" v-model.lazy="searchQuery"/>
+        <button class="search-button"> Search</button>
+    </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: "SearchComponent",
+    data() {
+      return {
+        searchQuery: '',
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    .search-container {
+        display: grid;
+        grid-template-columns: 1fr 200px;
+    }
+
+    .search-input {
+        width: auto;
+    }
+</style>
